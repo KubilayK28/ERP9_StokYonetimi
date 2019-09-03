6db0e85c1e42bac29ad560f09625cc18ed40a938
KubilayK28 <kubi.kirci@gmail.com>
2019-09-03 10:45:54 +0300

    Add project files.

---
+++ Sy.Core/Abstracts/AuditBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class AuditBase
+    {
+       
+        [StringLenght(50)]
+        public string CreatedUSer { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [StringLenght(50)]
+        public string UpdatedUser { get; set; }
+
+        public DateTime? UpdatedDate { get; set; } //null yapmak için soru işareti koyuyoruz
+
+
+    }
+}
---
+++ Sy.Core/Abstracts/StringLenghtAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sy.Core.Abstracts
+{
+    internal class StringLenghtAttribute : Attribute
+    {
+        private int v;
+
+        public StringLenghtAttribute(int v)
+        {
+            this.v = v;
+        }
+    }
+}
